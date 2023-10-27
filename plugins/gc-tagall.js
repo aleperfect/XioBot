7d2cd32cc0f6efeec2eae9c096a1f520053b32e8
aleizn <119465229+aleizn@users.noreply.github.com>
2023-10-27 13:46:46 -0500

    Aleizn

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,12 +5,10 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `¿ ‼️Deseas un bot para tu grupo‼️ ? Wa.me/51992621601
-👨🏻‍💻Deseas un bot de doxeo ? (+51) INFO AL NUMERO ALEIZN-MD
-*⺀👻Madrugar se hace más fácil contigo a mi lado⺀*\n\n🟩 ${oi}\n\n🟩 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let teks = `*⺀👻Madrugar se hace más fácil contigo a mi lado⺀*\n\n🟩 ${oi}\n\n🟩 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
 teks += `┊🐲 @${mem.id.split('@')[0]}\n`}
-teks += `➡️𝐀𝐥𝐞𝐢𝐳𝐧-𝐌𝐃`
+teks += `➡️𝐗𝐢𝐨 𝐁𝐨𝐭`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
